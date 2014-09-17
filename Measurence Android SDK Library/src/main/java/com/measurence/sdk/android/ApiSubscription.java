8d285249449f0d1eaa29366f2df4bd5da462d89b
Piercarlo Slavazza <pslavazza@measurence.com>
2014-09-17 15:49:26 +0200

    added RegistrationService

--- Measurence Android SDK Library/src/main/java/com/measurence/sdk/android/ApiSubscription.java
+++
@@ -1,9 +0,0 @@
-package com.measurence.sdk.android;
-
-public class ApiSubscription {
-
-    public static String applySubscription() {
-        throw new RuntimeException("not implemented");
-    }
-
-}
